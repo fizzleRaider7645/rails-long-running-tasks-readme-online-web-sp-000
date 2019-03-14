d5bbea6a8e423d012869f0b76286936ec6c43ce5
Douglas Uretsky <douglasuretsky@gmail.com>
2019-03-14 15:01:51 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Customer.create([{first_name: "Douglas", last_name: "Uretsky", email: "douglasuretsky@gmail.com"},
+  {first_name: "Stephen", last_name: "Garner", email: "stevieg@gmail.com"}])
